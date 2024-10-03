ef0026238de6c8e1c6e96ba717ae98998a0e0055
Mike-devoloper <ismoiljon.tursunov33@gmail.com>
2024-10-04 00:41:48 +0900

    fix: MUI customized integration

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,16 +2,21 @@ import { Button, Typography } from '@mui/material';
 import { Box, Container, Stack } from '@mui/system';
 import React from 'react';
 import '../css/app.css';
+import { RippleBadge } from './MaterialTheme/styled';
 
 function App() {
-  return <Container maxWidth="sm">
+  return <Container sx={{background: "orange"}}>
       <Stack flexDirection={"column"}>
         <Box sx={{my: 4}}>
           <Typography variant='h4' component={"h4"}>
             Create React App on TypeScript with Redux
           </Typography>
         </Box>
-        <Button variant="contained">Contained</Button>
+        <Box>
+          <RippleBadge badgeContent={4}> 
+          <Button variant="contained">Contained</Button>
+          </RippleBadge>
+        </Box>
       </Stack>
   </Container>
 }
