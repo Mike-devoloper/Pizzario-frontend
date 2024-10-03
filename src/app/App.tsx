1d35c9bbc12c8d940055e7485f8a4ae6d4f1d0b4
Mike-devoloper <ismoiljon.tursunov33@gmail.com>
2024-10-04 00:24:13 +0900

    feat: Material UI standart integration

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,19 @@
+import { Button, Typography } from '@mui/material';
+import { Box, Container, Stack } from '@mui/system';
 import React from 'react';
 import '../css/app.css';
 
 function App() {
-  return (
-   <div>Hello World Mike</div>
-  );
+  return <Container maxWidth="sm">
+      <Stack flexDirection={"column"}>
+        <Box sx={{my: 4}}>
+          <Typography variant='h4' component={"h4"}>
+            Create React App on TypeScript with Redux
+          </Typography>
+        </Box>
+        <Button variant="contained">Contained</Button>
+      </Stack>
+  </Container>
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './app/App';
 import reportWebVitals from './reportWebVitals';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
 import './css/index.css';
 
-
-
-ReactDOM.render(
+const container = document.getElementById('root')
+const root = createRoot(container!);
+root.render(
   <React.StrictMode>
     <Provider store={store}>
+    <ThemeProvider theme={theme}>
+    <CssBaseline />
       <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
